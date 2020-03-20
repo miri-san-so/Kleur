59408468fb0b769810899fb970027f6615df6590
miri <56790941+miri-san-so@users.noreply.github.com>
2020-03-20 12:34:45 +0530

    added canvas argument
    function setup ( can, x, img_obj )

--- Kleur.js
+++ Kleur.js
@@ -164,11 +164,11 @@ class Kleur {
     ];
   }
 
-  setup(x, img_obj) {
+  setup(can, x, img_obj) {
     x.canvas.width = img_obj.width;
     x.canvas.height = img_obj.height;
     x.drawImage(imageObj, 0, 0, img_obj.width, img_obj.height);
-    var imgData = x.getImageData(0, 0, canvas.width, canvas.height);
+    var imgData = x.getImageData(0, 0, can.width, can.height);
     var data = imgData.data;
     return data;
   }
@@ -240,7 +240,7 @@ class Kleur {
 
   getSwatch(color_arr) {
     let colors = [];
-    for (let i = 0; i < color_arr.length; i++) {
+    for (i = 0; i < color_arr.length; i++) {
       if (Math.abs(color_arr[i].lab[1] - color_arr[i].lab[2]) > 20) {
         light.push(color_arr[i]);
       }
